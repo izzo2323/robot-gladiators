44898086ca43fac49ea147f74090b0e5bff4727a
Nick Rizzo <nickrizzo77277@gmail.com>
2021-03-27 14:11:18 -0600

    added breaks to fix skip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,18 +8,19 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    while(enemyHealth > 0) {
+    while(playerHealth > 0 && enemyHealth > 0) {
     //window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
     if (promptFight === "fight" || promptFight === "FIGHT") {
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-        playerName + " attacked " + enemyName + ". " + enemyName + " now has" + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
     );
 
     if ( enemyHealth <=0) {
         window.alert(enemyName + " has died!")
+        break;
     }
     else {
         window.alert(enemyName + " still has " + enemyHealth + " health left.")
@@ -27,19 +28,22 @@ var fight = function(enemyName) {
     
     playerHealth = playerHealth - enemyAttack;
     console.log(
-        enemyName + " attacked " + playerName + "." + playerName + " now has" + playerHealth + " health remaining."
+        enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
     );
     if (playerHealth <=0) {
         window.alert(playerName + " had died!");
+        break;
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
 
-    } else if (promptFight === "skip" || promptFight === "SKIP") {
+    } if (promptFight === "skip" || promptFight === "SKIP") {
         var confirmSkip = window.confirm("Are you sure you'd like to quit?");
         if (confirmSkip) {
             window.alert(playerName + " had chosen to skip this fight. Goodbye!");
-            playerMoney = playerMoney - 2;
+            playerMoney = playerMoney - 10;
+            console.log("playerMoney", playerMoney);
+            break;
         }
         else {
             fight();
